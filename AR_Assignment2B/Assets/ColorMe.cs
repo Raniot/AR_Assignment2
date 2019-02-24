de45852eae189265f2bbe1c093edb48e82574b4e
Nicklas Nielsen <nnicklas92@gmail.com>
2019-02-24 16:11:35 +0100

    Added OpenCV

---
+++ AR_Assignment2B/Assets/ColorMe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OpenCVForUnity.Calib3dModule;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using Button = UnityEngine.UI.Button;
+
+public class ColorMe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
